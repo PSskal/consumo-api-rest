f607bd50dff83acbe345fdfd4e8a0ea0e77fdef6
Richard Pascual <ricpascual@gmail.com>
2023-02-03 16:56:50 -0500

    feat: :bug: manipulacion de DOM

--- src/assets/main.js
+++ src/assets/main.js
@@ -16,13 +16,20 @@ async function reloadMichis() {
   console.log({"reload":data});
 
   if (response.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status;
+    spanError.innerHTML = "Hubo un error: " + response.status;
   } else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+
 
     img1.src = data[0].url;
     img2.src = data[1].url;
+    console.log(typeof(data[0].id));
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
   }
 
 }
@@ -32,26 +39,43 @@ async function loadFavoritesMichis() {
   console.log({"favorites":data})
 
   if (response.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    spanError.innerHTML = "Hubo un error: " + response.status + data.message;
+  }else{
+    data.forEach(michi => {
+      const section = document.getElementById('favoriteMichis')
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
+
 }
-async function saveFavouriteMichis() {
-  const res = await fetch(`${API}v1/favourites`, {
+async function saveFavouriteMichi(id) {
+  const response = await fetch(`${API}v1/favourites`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'x-api-key': 'live_koCj0H2g9wNPIVErd1wfmGaDllbZDqMEQh9qmQiEI9uJalzfA03DGzmZk0m1D3lZ'
     },
     body: JSON.stringify({
-      image_id: 'dje'
+      "image_id": id
     }),
   });
-  const data = await res.json();
+  const data = await response.json();
 
   console.log('Save')
-  console.log(res)
+  console.log(response)
 
   if (res.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    spanError.innerHTML = "Hubo un error: " + response.status + data.message;
   }
 }
 
