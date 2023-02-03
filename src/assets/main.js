152760f0f4b61e0374be31e007a0b91e292cf701
Richard Pascual <ricpascual@gmail.com>
2023-02-03 16:14:29 -0500

    feat: :zap: endpoint y queryparameters
    Entendiendo un poco como utilizar los query parametes  y como utilizar los end points

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,10 +1,21 @@
 console.log('Hello, world')
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API = 'https://api.thecatapi.com/';
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-  });
+async function reload() {
+  const response = await fetch(`${API}v1/images/search?limit=3`);
+  const data = await response.json();
+  console.log({"reload":data});
+
+  const img1 = document.getElementById('img1');
+  const img2 = document.getElementById('img2');
+  const img3 = document.getElementById('img3');
+  
+
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+
+}
+
+reload();
